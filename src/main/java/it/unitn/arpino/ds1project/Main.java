0290cb1a13941030d9cb81126e35983349486b7a
andreastedile <andreastedile97@gmail.com>
2021-09-22 19:32:12 +0200

    Create a dummy ActorSystem

--- src/main/java/it/unitn/arpino/ds1project/Main.java
+++ src/main/java/it/unitn/arpino/ds1project/Main.java
@@ -1,7 +1,11 @@
 package it.unitn.arpino.ds1project;
 
+import akka.actor.ActorSystem;
+
 public class Main {
     public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("ds1project");
         System.out.println("Hello, world!");
+        system.terminate();
     }
 }
