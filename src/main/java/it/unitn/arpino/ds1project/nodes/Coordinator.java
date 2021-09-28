49b3122ad4fed8f9df7272104e801999f776c3fc
andreastedile <andreastedile97@gmail.com>
2021-09-28 10:05:56 +0200

    Add yesVoters field

--- src/main/java/it/unitn/arpino/ds1project/nodes/Coordinator.java
+++ src/main/java/it/unitn/arpino/ds1project/nodes/Coordinator.java
@@ -1,5 +1,6 @@
 package it.unitn.arpino.ds1project.nodes;
 
+import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 import it.unitn.arpino.ds1project.transaction.messages.TxnAcceptMsg;
@@ -8,19 +9,18 @@ import it.unitn.arpino.ds1project.transaction.messages.TxnEndMsg;
 import it.unitn.arpino.ds1project.twopc.CoordinatorFSM;
 import it.unitn.arpino.ds1project.twopc.messages.VoteRequest;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Coordinator extends AbstractNode {
     private final List<Integer> clientIds;
     private final Map<Integer, CoordinatorFSM> twoPcFSM;
+    private final Map<Integer, Set<ActorRef>> yesVoters;
 
     public Coordinator(int id) {
         super(id);
         clientIds = new ArrayList<>();
         twoPcFSM = new HashMap<>();
+        yesVoters = new HashMap<>();
     }
 
     @Override
