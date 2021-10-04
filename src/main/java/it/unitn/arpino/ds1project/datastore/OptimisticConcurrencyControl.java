733045dc666c9b9de34d0cbb125b710b669f44b9
andreastedile <andreastedile97@gmail.com>
2021-10-04 15:47:27 +0200

    Improve documentation and method names

--- src/main/java/it/unitn/arpino/ds1project/datastore/OptimisticConcurrencyControl.java
+++ src/main/java/it/unitn/arpino/ds1project/datastore/OptimisticConcurrencyControl.java
@@ -17,7 +17,7 @@ public class OptimisticConcurrencyControl {
     }
 
     public boolean prepare(Transaction transaction) {
-        if (!onPar(transaction)) {
+        if (!onParWithDatabase(transaction)) {
             return false;
         }
         if (!acquireLocks(transaction)) {
@@ -29,20 +29,41 @@ public class OptimisticConcurrencyControl {
 
     public void commit(Transaction transaction) {
         Workspace workspace = transaction.getWorkspace();
-        workspace.getKeys().forEach(key -> database.write(key, workspace.read(key)));
+
+        // for each data item the transaction has read or written:
+        workspace.getKeys().forEach(key -> {
+
+            // write back the data item's value into the database
+            database.write(key, workspace.read(key));
+
+            // update the data item's version
+            database.setVersion(key, database.version(key) + 1);
+        });
+
+        // release all locks held by the transaction
         transactionRepository.getLocks(transaction).forEach(lockRepository::release);
+
+        // add the transaction to the list of committed ones
         transactionRepository.setCommitted(transaction);
     }
 
     public void abort(Transaction transaction) {
+        // release all locks held by the transaction
         transactionRepository.getLocks(transaction).forEach(lockRepository::release);
+
+        // add the transaction to the list of aborted ones
         transactionRepository.setAborted(transaction);
     }
 
     /**
-     * Verifies whether the data items in the database have not undergone updates in the meanwhile of the transaction
+     * Verifies that the versions of the data items that the transaction has read or written are not "dated"
+     * with respect to the versions of the data items in the database. This can happen if another transaction
+     * which has read or written the same data items has already committed in the meanwhile.
+     *
+     * @return true if all versions of the transaction's data items are on par with the database's data items,
+     * false otherwise.
      */
-    private boolean onPar(Transaction transaction) {
+    private boolean onParWithDatabase(Transaction transaction) {
         Workspace workspace = transaction.getWorkspace();
 
         return workspace.getKeys().stream()
@@ -50,7 +71,9 @@ public class OptimisticConcurrencyControl {
     }
 
     /**
-     * Attempts to lock all the data items that the transaction has read or written
+     * Either locks all data items that the transaction has read or written, or none.
+     *
+     * @return true if all data items have been locked, false if none of the data items have been locked.
      */
     private boolean acquireLocks(Transaction transaction) {
         Workspace workspace = transaction.getWorkspace();
@@ -65,7 +88,18 @@ public class OptimisticConcurrencyControl {
         }
 
         locks.forEach(Lock::unlock);
-        // Todo
         return false;
+    }
+
+    protected int read(int key) {
+        return database.read(key);
+    }
+
+    protected void write(int key, int value) {
+        database.write(key, value);
+    }
+
+    protected int version(int key) {
+        return database.version(key);
     }
 }
