d17e22434001c04a0952a91e51553926cf209764
ArpiNatoR <nicolatosh4@gmail.com>
2021-10-30 11:23:22 +0200

    Add Timeout

---
+++ src/main/java/it/unitn/arpino/ds1project/messages/coordinator/TimeoutExpired.java
@@ -0,0 +1,28 @@
+package it.unitn.arpino.ds1project.messages.coordinator;
+
+import it.unitn.arpino.ds1project.messages.Message;
+import it.unitn.arpino.ds1project.messages.Transactional;
+
+import java.util.UUID;
+
+/**
+ * This is a message that the coordinator sends to itself indicating that the timeout duration for collecting
+ * all VoteResponses has elapsed. Upon receiving it, the coordinator should abort the ongoing transaction.
+ */
+public class TimeoutExpired extends Message implements Transactional {
+    private UUID uuid;
+
+    public TimeoutExpired(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public Message.TYPE getType() {
+        return TYPE.TwoPC;
+    }
+
+    @Override
+    public UUID uuid() {
+        return uuid;
+    }
+}
