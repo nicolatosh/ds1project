63b83838bbb560b6278507904d6d7a42942308b7
andreastedile <andrea.stedile@studenti.unitn.it>
2021-10-30 14:25:06 +0200

    Remove list of expired transactions (misleading)

--- src/main/java/it/unitn/arpino/ds1project/nodes/context/ContextManager.java
+++ src/main/java/it/unitn/arpino/ds1project/nodes/context/ContextManager.java
@@ -11,13 +11,11 @@ import java.util.stream.Stream;
 public class ContextManager<T extends RequestContext> {
     protected final List<T> active;
     protected final List<T> completed;
-    protected final List<T> expired;
 
 
     public ContextManager() {
         this.active = new ArrayList<>();
         this.completed = new ArrayList<>();
-        this.expired = new ArrayList<>();
     }
 
     /**
@@ -45,22 +43,8 @@ public class ContextManager<T extends RequestContext> {
         completed.add(context);
     }
 
-    /**
-     * @return The contexts of the transactions which have exceeded the maximum time for which they were
-     * allowed to stay in an incomplete state.
-     */
-    public List<T> getExpired() {
-        return expired;
-    }
-
-    public void setExpired(T context) {
-        active.remove(context);
-        context.setStatus(RequestContext.Status.EXPIRED);
-        expired.add(context);
-    }
-
     public Optional<T> contextOf(Transactional msg) {
-        return Stream.of(active, completed, expired)
+        return Stream.of(active, completed)
                 .flatMap(Collection::stream)
                 .filter(ctx -> ctx.uuid == msg.uuid())
                 .findFirst();
