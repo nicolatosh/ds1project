ba01ecf5e5779c89a3d4c0bc5b04d13846046d72
andreastedile <andrea.stedile@studenti.unitn.it>
2021-11-11 18:46:03 +0100

    Add more tests

--- src/test/java/it/unitn/arpino/ds1project/nodes/DataStoreNodeTest.java
+++ src/test/java/it/unitn/arpino/ds1project/nodes/DataStoreNodeTest.java
@@ -1,17 +1,21 @@
 package it.unitn.arpino.ds1project.nodes;
 
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 import akka.testkit.TestActorRef;
 import akka.testkit.TestKit;
-import it.unitn.arpino.ds1project.messages.Message;
+import it.unitn.arpino.ds1project.nodes.context.RequestContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class DataStoreNodeTest {
     ActorSystem system;
@@ -30,17 +34,42 @@ class DataStoreNodeTest {
     }
 
     @Test
+    @Order(1)
+    void testNoDuplicateContexts() {
+        SimpleRequestContext ctx = new SimpleRequestContext(UUID.randomUUID());
+        node.underlyingActor().addContext(ctx);
+        node.underlyingActor().addContext(ctx);
+        assertSame(1, node.underlyingActor().getActive().size());
+    }
+
+    @Test
+    @Order(2)
+    void testGetContext() {
+        SimpleRequestContext ctx = new SimpleRequestContext(UUID.randomUUID());
+        node.underlyingActor().addContext(ctx);
+        assertEquals(1, node.underlyingActor().getActive().size());
+        assertEquals(0, node.underlyingActor().getDecided().size());
+        ctx.decided = true;
+        assertEquals(0, node.underlyingActor().getActive().size());
+        assertEquals(1, node.underlyingActor().getDecided().size());
+    }
+
+    @Test
+    @Order(3)
     void crashAndResume() {
         new TestKit(system) {
             {
+                assertSame(DataStoreNode.Status.ALIVE, node.underlyingActor().getStatus());
                 node.tell("Hello", testActor());
                 expectMsg("World");
 
                 node.underlyingActor().crash();
+                assertSame(DataStoreNode.Status.CRASHED, node.underlyingActor().getStatus());
                 node.tell("Hello", testActor());
                 expectNoMessage();
 
                 node.underlyingActor().resume();
+                assertSame(DataStoreNode.Status.ALIVE, node.underlyingActor().getStatus());
                 node.tell("Hello", testActor());
                 expectMsg("World");
             }
@@ -48,8 +77,7 @@ class DataStoreNodeTest {
     }
 
 
-    @SuppressWarnings("rawtypes")
-    private static class SimpleNode extends DataStoreNode {
+    private static class SimpleNode extends DataStoreNode<SimpleRequestContext> {
         public static Props props() {
             return Props.create(SimpleNode.class, SimpleNode::new);
         }
@@ -59,6 +87,19 @@ class DataStoreNodeTest {
             return ReceiveBuilder.create()
                     .matchEquals("Hello", msg -> getSender().tell("World", getSelf()))
                     .build();
+        }
+    }
+
+    private static class SimpleRequestContext extends RequestContext {
+        private boolean decided;
+
+        public SimpleRequestContext(UUID uuid) {
+            super(uuid);
+        }
+
+        @Override
+        public boolean isDecided() {
+            return decided;
         }
     }
 }
