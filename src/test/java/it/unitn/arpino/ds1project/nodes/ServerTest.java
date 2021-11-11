5d5f69c587ad219cef1d6d0ebe3300609c6bec0d
andreastedile <andrea.stedile@studenti.unitn.it>
2021-11-11 19:38:10 +0100

    Simplifications

--- src/test/java/it/unitn/arpino/ds1project/nodes/ServerTest.java
+++ src/test/java/it/unitn/arpino/ds1project/nodes/ServerTest.java
@@ -13,15 +13,11 @@ import it.unitn.arpino.ds1project.messages.server.VoteRequest;
 import it.unitn.arpino.ds1project.messages.server.WriteRequest;
 import it.unitn.arpino.ds1project.nodes.server.Server;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ServerTest {
     ActorSystem system;
@@ -52,14 +48,11 @@ public class ServerTest {
                 // The transaction writes a value, which affects the transaction's own private workspace.
 
                 UUID uuid1 = UUID.randomUUID();
-                WriteRequest write1 = new WriteRequest(uuid1, 6, 742);
-                server.tell(write1, coord);
+                server.tell(new WriteRequest(uuid1, 6, 742), coord);
                 expectNoMessage();
 
-                ReadRequest read1 = new ReadRequest(uuid1, 6);
-                server.tell(read1, coord);
-                ReadResult result1 = expectMsgClass(ReadResult.class);
-                Assertions.assertEquals(new ReadResult(uuid1, 6, 742), result1);
+                server.tell(new ReadRequest(uuid1, 6), coord);
+                expectMsg(new ReadResult(uuid1, 6, 742));
 
                 // Mock the first transaction, mocking the messages from a coordinator.
                 // The transaction does a read of the same element written by the first transaction.
@@ -68,19 +61,14 @@ public class ServerTest {
                 // written by the first transaction.
 
                 UUID uuid2 = UUID.randomUUID();
-                ReadRequest read2 = new ReadRequest(uuid2, 6);
-                server.tell(read2, coord);
-                ReadResult result2 = expectMsgClass(ReadResult.class);
-                assertNotEquals(new ReadResult(uuid2, 6, 742), result2);
-                assertEquals(new ReadResult(uuid2, 6, DatabaseBuilder.DEFAULT_DATA_VALUE), result2);
+                server.tell(new ReadRequest(uuid2, 6), coord);
+                expectMsg(new ReadResult(uuid2, 6, DatabaseBuilder.DEFAULT_DATA_VALUE));
 
                 // The first transaction commits. The value in the transaction's private workspace
                 // is copied into the database.
 
-                VoteRequest voteRequest1 = new VoteRequest(uuid1);
-                server.tell(voteRequest1, coord);
-                VoteResponse voteResponse1 = expectMsgClass(VoteResponse.class);
-                assertEquals(new VoteResponse(uuid1, VoteResponse.Vote.YES), voteResponse1);
+                server.tell(new VoteRequest(uuid1), coord);
+                expectMsg(new VoteResponse(uuid1, VoteResponse.Vote.YES));
 
                 FinalDecision decision1 = new FinalDecision(uuid1, FinalDecision.Decision.GLOBAL_COMMIT);
                 server.tell(decision1, coord);
@@ -89,30 +77,23 @@ public class ServerTest {
                 // The second transaction must not perceive the change introduced by the commit:
                 // it is still working on its own private workspace.
 
-                server.tell(read2, coord);
-                result2 = expectMsgClass(ReadResult.class);
-                assertNotEquals(new ReadResult(uuid2, 6, 742), result2);
-                assertEquals(new ReadResult(uuid2, 6, DatabaseBuilder.DEFAULT_DATA_VALUE), result2);
+                server.tell(new ReadRequest(uuid2, 6), coord);
+                expectMsg(new ReadResult(uuid2, 6, DatabaseBuilder.DEFAULT_DATA_VALUE));
 
                 // Mock the third transaction, mocking the messages from a coordinator.
                 // The transaction does a read of the same element written by the first transaction.
                 // In this case, the read value must be the committed value.
 
                 UUID uuid3 = UUID.randomUUID();
-                ReadRequest read3 = new ReadRequest(uuid3, 6);
-                server.tell(read3, coord);
-                ReadResult result3 = expectMsgClass(ReadResult.class);
-                assertEquals(new ReadResult(uuid3, 6, 742), result3);
+                server.tell(new ReadRequest(uuid3, 6), coord);
+                expectMsg(new ReadResult(uuid3, 6, 742));
 
                 // The second transaction attempts commits. The response must be negative.
 
-                VoteRequest voteRequest2 = new VoteRequest(uuid2);
-                server.tell(voteRequest2, coord);
-                VoteResponse voteResponse2 = expectMsgClass(VoteResponse.class);
-                assertEquals(new VoteResponse(uuid2, VoteResponse.Vote.NO), voteResponse2);
+                server.tell(new VoteRequest(uuid2), coord);
+                expectMsg(new VoteResponse(uuid2, VoteResponse.Vote.NO));
 
-                FinalDecision decision2 = new FinalDecision(uuid2, FinalDecision.Decision.GLOBAL_ABORT);
-                server.tell(decision2, coord);
+                server.tell(new FinalDecision(uuid2, FinalDecision.Decision.GLOBAL_ABORT), coord);
                 expectNoMessage();
             }
         };
