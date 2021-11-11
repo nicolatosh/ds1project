28170e2a4f2bd471cfc4a237733cd2cc9561e8cc
andreastedile <andrea.stedile@studenti.unitn.it>
2021-11-11 18:45:14 +0100

    Test RequestContext's equals

---
+++ src/test/java/it/unitn/arpino/ds1project/nodes/context/RequestContextTest.java
@@ -0,0 +1,28 @@
+package it.unitn.arpino.ds1project.nodes.context;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class RequestContextTest {
+    @Test
+    void testEquals() {
+        RequestContext ctx1 = new RequestContext(UUID.randomUUID()) {
+            @Override
+            public boolean isDecided() {
+                return false;
+            }
+        };
+
+        RequestContext ctx2 = new RequestContext(UUID.randomUUID()) {
+            @Override
+            public boolean isDecided() {
+                return false;
+            }
+        };
+
+        assertNotEquals(ctx1, ctx2);
+    }
+}
