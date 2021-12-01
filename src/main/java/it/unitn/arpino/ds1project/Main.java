2e84816bf33b1a479bb3c36546c107edb7fbb10d
andreastedile <andrea.stedile@studenti.unitn.it>
2021-12-01 17:30:46 +0100

    Refactor main

--- src/main/java/it/unitn/arpino/ds1project/Main.java
+++ src/main/java/it/unitn/arpino/ds1project/Main.java
@@ -12,41 +12,34 @@ import it.unitn.arpino.ds1project.nodes.server.Server;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        final ActorSystem system = ActorSystem.create("ds1project");
+    public static void main(String[] args) {
+        var system = ActorSystem.create("ds1project");
 
         // setup servers, coordinators and clients
 
-        final ActorRef coord1 = system.actorOf(Coordinator.props(), "coord1");
-        final ActorRef coord2 = system.actorOf(Coordinator.props(), "coord2");
-
-        final ActorRef client1 = system.actorOf(TxnClient.props(1), "client1");
-        final ActorRef client2 = system.actorOf(TxnClient.props(2), "client2");
-
-        List<ActorRef> coordinatorList = List.of(coord1, coord2);
-        ClientStartMsg clientMsg = new ClientStartMsg(coordinatorList, 19);
-        client1.tell(clientMsg, ActorRef.noSender());
-        client2.tell(clientMsg, ActorRef.noSender());
-
-        final ActorRef server0 = system.actorOf(Server.props(0, 9), "server0");
-        final ActorRef server1 = system.actorOf(Server.props(10, 19), "server1");
-
+        var server0 = system.actorOf(Server.props(0, 9), "server0");
+        var server1 = system.actorOf(Server.props(10, 19), "server1");
         server0.tell(new JoinMessage(10, 19), server1);
         server1.tell(new JoinMessage(0, 9), server0);
 
+        var coord0 = system.actorOf(Coordinator.props(), "coord0");
+        var coord1 = system.actorOf(Coordinator.props(), "coord1");
+        coord0.tell(new JoinMessage(0, 9), server0);
+        coord0.tell(new JoinMessage(10, 19), server1);
         coord1.tell(new JoinMessage(0, 9), server0);
         coord1.tell(new JoinMessage(10, 19), server1);
-        coord2.tell(new JoinMessage(0, 9), server0);
-        coord2.tell(new JoinMessage(10, 19), server1);
 
+        var start = new StartMessage();
+        server0.tell(start, ActorRef.noSender());
+        server1.tell(start, ActorRef.noSender());
+        coord0.tell(start, ActorRef.noSender());
+        coord1.tell(start, ActorRef.noSender());
 
-        server0.tell(new StartMessage(), ActorRef.noSender());
-        server1.tell(new StartMessage(), ActorRef.noSender());
-        coord1.tell(new StartMessage(), ActorRef.noSender());
-        coord2.tell(new StartMessage(), ActorRef.noSender());
+        var client0 = system.actorOf(TxnClient.props(1), "client0");
+        var client1 = system.actorOf(TxnClient.props(2), "client1");
 
-        Thread.sleep(200);
-
-        system.terminate();
+        var clientStart = new ClientStartMsg(List.of(coord0, coord1), 19);
+        client0.tell(clientStart, ActorRef.noSender());
+        client1.tell(clientStart, ActorRef.noSender());
     }
 }
